8a0209f3c776c5c586cb0fd8a437fb8cb514c055
Caike Cunha <techcaikebr@gmail.com>
2025-02-05 21:42:33 -0300

    Adicionado __main__, definido porta e comenta geração de números

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import random
 app = Flask(__name__)
 
 def validador():
+    # Sorteia um número entre 1 e 10, caso o número seja PAR retorna True como autorizado
     NUMERO = random.randint(1, 10)
-    return NUMERO % 2 == 0
+    return NUMERO % 2 == 0  # Retorno booleano, True (par) ou False (ímpar)
 
 @app.route('/')
 def index():
@@ -15,4 +16,5 @@ def index():
         'message': 'autorizado' if AUTORIZADO else 'negado'
     }), 200 if AUTORIZADO else 403  # 200 para autorizado, 403 para negado
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
