f2ef0e6f69de2b7e5ce2ea5c8e6b3e2e97610aaa
Caike Cunha <techcaikebr@gmail.com>
2025-02-06 12:54:06 -0300

    Criado um endpoint para sempre negado, e outro para sempre autorizado

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def validador():
     valores = (True, False)
     return random.choice(valores)  # Retorna o booleano de forma aleatória
 
+# Endpoint que retorna autorizado ou negado aleatoriamente
 @app.route('/')
 def index():
     AUTORIZADO = validador()
@@ -15,6 +16,22 @@ def index():
         'status': 'ok' if AUTORIZADO else 'falha',
         'message': 'autorizado' if AUTORIZADO else 'negado'
     }), 200 if AUTORIZADO else 403  # 200 para autorizado, 403 para negado
+
+# Endpoint que sempre retorna autorizado
+@app.route('/sempre-autorizado')
+def sempre_autorizado():
+    return jsonify({
+        'status': 'ok',
+        'message': 'autorizado'
+    }), 200
+
+# Endpoint que sempre retorna negado
+@app.route('/sempre-negado')
+def sempre_negado():
+    return jsonify({
+        'status': 'falha',
+        'message': 'negado'
+    }), 403
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
